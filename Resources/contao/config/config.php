3f498214bb5e4fe45631e22d3bd03713981a47a8
Sven Baumann <baumann.sv@gmail.com>
2016-10-27 10:20:34 +0200

    create it

---
+++ Resources/contao/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Copyright © ContaoBlackForest
+ *
+ * @package   contao-system-notification
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   GNU/LGPL
+ * @copyright Copyright 2014-2016 ContaoBlackForest
+ */
