e6253c9c8dcb56eea5757992fab9ab16c9f412ec
Sven Baumann <baumann.sv@gmail.com>
2016-11-11 10:56:27 +0100

    add base configuration

--- Resources/contao/config/config.php
+++ Resources/contao/config/config.php
@@ -9,3 +9,26 @@
  * @license   GNU/LGPL
  * @copyright Copyright 2014-2016 ContaoBlackForest
  */
+
+/**
+ * Add the backend module.
+ */
+$systemModules = array();
+
+foreach ($GLOBALS['BE_MOD']['system'] as $name => $configuration) {
+    $systemModules[$name] = $configuration;
+
+    if ($name === 'log') {
+        $systemModules['system_notification']['tables'] =
+            array('tl_system_notification', 'tl_system_notification_message');
+        $systemModules['system_notification']['icon'] ='assets/system-notification/images/backend/error-icon.png';
+    }
+}
+
+$GLOBALS['BE_MOD']['system'] = $systemModules;
+
+
+/**
+ * Activate the fatal error controller.
+ */
+new ContaoBlackForest\SystemNotification\Controller\FatalErrorController();
